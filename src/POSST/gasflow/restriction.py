db30ff0ebdac40804d9d519ecba7f5094544b8da
N.Nickig <niklas.nickig@outlook.de>
2024-11-08 14:25:48 +0100

    restriction docu

--- src/POSST/gasflow/restriction.py
+++ src/POSST/gasflow/restriction.py
@@ -1,11 +1,28 @@
 from POSST.misc import Gas
 
 class model_1():
-    """Calculate Massflow"""
+    r"""
+    ## Description
+    Model 1 for Massflow calculation between two plena.
+    Assumed as a orifice between the plena.
 
-    def __init__(self, name="Massflow", l=0.1, b=1, h=1, R=8.314, m=0, dt=1e-3):
-        """
-        Annahme Viskoität & Dichte immer kons
+    ### Calculations
+
+        $$ m= \rho \cdot A \cdot v_{orifice} $$
+    """
+
+    def __init__(self, name="Massflow", l=0.1, b=1, h=1, R=287, m=0, dt=1e-3):
+        r"""
+        ## Arguments
+            l: length of modeled pipe in [$m$]
+            b: width of modeled pipe in [$m$]
+            h: height of modeled pipe in [$m$]
+            R: gas constant in [$J \cdot Kg^{-1} \cdot K^{-1}}$]
+            m: initial assumed mass flow in [$Kg^{-1} \cdot s^{-1}$]
+            dt: time step in [$s$]
+
+        ## Returns
+           Instance of orifice model 1
         """
         self.b = b
         self.h = h
@@ -21,7 +38,20 @@ class model_1():
         self.CNat = Gas.CNat
         self.v = 1e-13
 
-    def __call__(self, p_in, p_out, T_in, x_O2=0.23, n_N2=0.77, x_H2=0, x_H2O=0):
+    def __call__(self, p_in, p_out, T_in, x_O2=0.23, x_N2=0.77, x_H2=0, x_H2O=0):
+        r"""
+        ## Arguments
+            p_in: inlet pressure in [$Pa$]
+            p_out: outlet pressure in [$Pa$]
+            T_in: inlet temperature in [$K$]
+            x_O2: O2 mass fraction in [$-$]
+            x_N2: N2 mass fraction in [$-$]
+            x_H2: H2 mass fraction in [$-$]
+            x_H2O: H2O mass fraction in [$-$]
+
+        ## Returns
+            m: massflow in [$Kg \cdot s^{-1}$]
+        """
         # try:
         rho_t = 1.2
         vis_t = 13.3 * 1e-6
@@ -32,7 +62,7 @@ class model_1():
         rho_i = []
         xn_i = []
         nO2_in = self.m * x_O2 / Gas.CNat.M_O2
-        nN2_in = self.m * n_N2 / Gas.CNat.M_N2
+        nN2_in = self.m * x_N2 / Gas.CNat.M_N2
         nH2_in = self.m * x_H2 / Gas.CNat.M_H2
         nH2O_in = self.m * x_H2O / Gas.CNat.M_H2O
         if self.m != 0:
@@ -67,13 +97,35 @@ class model_1():
             #        *math.sqrt(abs(self.delta_P))  \
             #        *self.delta_P/abs(self.delta_P)#p_vor*m_vor/p_nach
             self.v = self.Gas.v_2(self, p_in=self.p_vor, p_out=self.p_nach, rho=self.rho, l=self.l, D_H=self.D_h, Re=self.Re, zetha=0.4)
-            self.m = self.rho * self.A * self.v * self.dt
+            self.m = self.rho * self.A * self.v# * self.dt
         else:
             self.m = 0
         None
 
 class model_2():
+    r"""
+    ## Description
+    Model 2 for Massflow calculation between two plena.
+    Assumed as a orifice between the plena.
+
+    ### Calculations
+        $$ m = a_{coeff} \cdot A_{orifice} \cdot p_{in} \cdot \sqrt{}\frac{2}{ R_{s} \cdot T_{in}}} \cdot \Psi $$
+        with
+        $$ \Psi_{sonic} = \left( \frac{2}{\kappa + 1} \right)^{\frac{1}{\kappa - 1}} \cdot \sqrt{\frac{\kappa}{kappa + 1}} $$
+        $$ \Psi_{subsonic} = \sqrt{\frac{\kappa}{\kappa - 1}\cdot \left(\frac{p_{out}}{p_{in}}\right)^{\frac{2}{\kappa}} - \left(\frac{p_{out}}{p_{in}}\right)^{\frac{\kappa + 1}{\kappa}}}  $$
+    """
+
     def __init__(self, dt, A, R=287, cp=1000):
+        r"""
+        ## Arguments
+            A: orifice area in [$m^2$]
+            R: gas constant in [$J \cdot Kg^{-1} \cdot K^{-1}}$]
+            cp: specific heat capacity in [$J \cdot Kg^{-1} \cdot K^{-1}}$]
+            dt: time step in [$s$]
+
+        ## Returns
+           Instance of orifice model 2
+        """
         self.dt=dt
         self.A=A
         self.R=R
@@ -84,6 +136,17 @@ class model_2():
         self.m = 0
 
     def __call__(self, p_in, p_out, T_in, coef_flow=1):
+        r"""
+        ## Arguments
+            p_in: inlet pressure in [$Pa$]
+            p_out: outlet pressure in [$Pa$]
+            T_in: inlet temperature in [$K$]
+            coef_flow: flow coefficient in [$-$]
+
+        ## Returns
+            m: massflow in [$Kg \cdot s^{-1}$]
+        """
+
         dp = p_in - p_out
 
         if dp != 0:
